747d73aca8d26575c45de6021287aeaf86e55ee0
Ealtunlu <75951666+Ealtunlu@users.noreply.github.com>
2021-02-04 19:22:22 +0300

    Update day_4.py

--- Homeworks./day_4.py
+++ Homeworks./day_4.py
@@ -1,10 +1,10 @@
 #Finding prime numbers between 0 and 100 using functions
-def is_prime(n):
+def is_pirme(n):
     if n == 0 or n == 1:
          return False
     for x in range(2, n):
         if n % x == 0 :
             return False
     return True
-prime_nums = [x for x in range(0,100) if is_prime(x)]
-print(prime_nums)
+prime_nums = [x for x in range(0,100) if is_pirme(x)]
+print(f"Prime Numbers : {prime_nums}")
